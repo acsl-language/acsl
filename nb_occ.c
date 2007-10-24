26bc7f99c0c58d7f32c1ca204cd816a6ed358d4f
Claude Marché <claude.marche@inria.fr>
2007-10-24 13:23:20 +0000

    ex

---
+++ nb_occ.c
@@ -0,0 +1,24 @@
+
+
+/* nb_occ(t,i,j,e) gives the number of occurrences of e in t[i..j]
+ */
+
+//@ logic integer nb_occ<L>(double t[], integer i, integer j, double e);
+
+/*@ axiom nb_occ_empty<L> :
+  @   \forall double t[], integer i, integer j, double e;
+  @     i > j ==> nb_occ<L>(t,i,j,e) == 0;
+  @*/
+
+/*@ axiom nb_occ_true<L> :
+  @   \forall double t[], integer i, integer j, double e;
+  @     i <= j && \at(t[i],L) == e ==> 
+  @       nb_occ<L>(t,i,j,e) == nb_occ<L>(t,i,j-1,e) + 1;
+  @*/
+
+/*@ axiom nb_occ_false<L> :
+  @   \forall double t[], integer i, integer j, double e;
+  @     i <= j && \at(t[i],L) != e ==> 
+  @       nb_occ<L>(t,i,j,e) == nb_occ<L>(t,i,j-1,e);
+  @*/
+
