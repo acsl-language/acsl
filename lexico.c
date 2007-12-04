4a399288d404b2b6ad77f9e73c727b0d3d3d9ed7
Claude Marché <claude.marche@inria.fr>
2007-12-04 13:49:13 +0000

    spec

---
+++ lexico.c
@@ -0,0 +1,24 @@
+//@ ensures \result >= 0
+int dummy();
+
+//@ type intpair = (int,int)
+
+/*@ predicate lexico(intpair p1, intpair p2) {
+  @   \let (x1,y1) = p1 ;
+  @   \let (x2,y2) = p2 ;
+  @      x1 < y1 || x1 == y1 && x2 < y2
+  @*/
+
+//@ requires x >= 0 && y >= 0
+int f(int x,int y) {
+  /*@ invariant x >= 0 && y >= 0
+    @ variant (x,y) for lexico
+    @*/
+  while (x > 0 && y > 0) {
+    
+    if (dummy()) {
+      x--; y = dummy();
+    }
+    else y--;
+  }
+}
