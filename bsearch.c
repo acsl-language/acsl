42218f367baf51e012d2af2fea1e39d466154149
Claude Marché <claude.marche@inria.fr>
2007-12-11 16:50:18 +0000

    m

--- bsearch.c
+++ bsearch.c
@@ -1,12 +1,12 @@
-/*@ requires 
-  @   n >= 0 && \valid(t+(0..n-1)) &&
-  @   t_is_sorted : \forall short k1, short k2; 
-  @       0 <= k1 <= k2 <= n-1 ==> t[k1] <= t[k2];
+/*@ requires n >= 0 && \valid(t+(0..n-1));
   @ assigns \nothing;
+  @ ensures -1 <= \result <= n-1;
   @ behavior success:
   @   ensures \result >= 0 ==> t[\result] == v;
   @ behavior failure:
+  @   assumes (t_is_sorted : \forall integer k1, integer k2; 
+  @              0 <= k1 <= k2 <= n-1 ==> t[k1] <= t[k2];
   @   ensures \result == -1 ==> 
-  @     \forall short k; 0 <= k < n ==> t[k] != v;
+  @     \forall integer k; 0 <= k < n ==> t[k] != v;
   @*/
-short bsearch(int t[], short n, int v);
+int bsearch(double t[], int n, double v);
--- bsearch2.c
+++ bsearch2.c
@@ -1,23 +1,22 @@
-/*@ requires 
-  @   n >= 0 && \valid(t+(0..n-1)) &&
-  @   t_is_sorted :: \forall short k1, short k2; 
-  @       0 <= k1 <= k2 <= n-1 => t[k1] <= t[k2];
+/*@ requires n >= 0 && \valid(t+(0..n-1));
   @ assigns \nothing;
+  @ ensures -1 <= \result <= n-1;
   @ behavior success:
   @   ensures \result >= 0 ==> t[\result] == v;
   @ behavior failure:
+  @   assumes t_is_sorted : \forall integer k1, int k2; 
+  @       0 <= k1 <= k2 <= n-1 ==> t[k1] <= t[k2];
   @   ensures \result == -1 ==> 
-  @     \forall short k; 0 <= k < n => t[k] != v;
+  @     \forall integer k; 0 <= k < n => t[k] != v;
   @*/
-short bsearch(int t[], short n, int v) {
-  short l = 0, u = n-1;
+int bsearch(double t[], int n, double v) {
+  int l = 0, u = n-1;
   /*@ loop invariant 0 <= l && u <= n-1;   
     @ for failure: loop invariant  
-    @   \forall short k; 0 <= k < n => t[k] == v => l <= k <= u;
-    @ loop variant u-l; 
+    @   \forall integer k; 0 <= k < n && t[k] == v ==> l <= k <= u;
     @*/
   while (l <= u ) {
-    short m = l + (u-l)/2; // better than (u+l)/2
+    int m = l + (u-l)/2; // better than (u+l)/2
     if (t[m] < v) l = m + 1;
     else if (t[m] > v) u = m - 1;
     else return m; 
