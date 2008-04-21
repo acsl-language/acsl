6e82d288686286375cc96eaa7cc6db2511de82ff
Virgile Prévosto <virgile.prevosto@cea.fr>
2008-04-21 15:56:28 +0000

    terminates

--- assigns_array.c
+++ assigns_array.c
@@ -1,8 +1,9 @@
-/*@ assigns t[0..n-1];
+/*@ requires \valid(t+(0..n-1));
+  @ assigns t[0..n-1];
   @ assigns *(t+(0..n-1));
-  @ assigns *(t+{ i | integer i ; 0 <= i < n }); 
+  @ assigns *(t+{ i | integer i ; 0 <= i < n });
   @*/
-void reset_array(int t[],int n) { 
+void reset_array(int t[],int n) {
   int i;
   for (i=0; i < n; i++) t[i] = 0;
 }
