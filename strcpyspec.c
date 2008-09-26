ac1929078f05aa050f7d61fde4943ff607cbbf5d
Virgile Prévosto <virgile.prevosto@cea.fr>
2008-09-26 12:17:55 +0000

    compatibility of jessie plugin with why + fix typo in acsl manual

--- strcpyspec.c
+++ strcpyspec.c
@@ -1,12 +1,10 @@
 /*@ // src and dest cannot overlap
-  @ requires \base_addr(src) != \base_addr(dest); 
+  @ requires \base_addr(src) != \base_addr(dest);
   @ // src is a valid C string
-  @ requires \strlen(src) >= 0 ; 
+  @ requires \strlen(src) >= 0 ;
   @ // dest is large enough to store a copy of src up to the 0
-  @ requires \valid_range(dest,0,\strlen(src)); 
+  @ requires \valid_range(dest,0,\strlen(src));
   @ ensures
   @   \forall integer k; 0 <= k <= \strlen(src) ==> dest[k] == src[k]
   @*/
-void str_cpy(char *dest, const char *src);
-
-
+void strcpy(char *dest, const char *src);
