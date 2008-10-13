1555a0b186637a93bf1912ed43d7900b231a7e79
Patrick Baudin <patrick.baudin@cea.fr>
2008-10-13 08:45:48 +0000

    utilisation de \old dans les clauses breaks, continues et returns.

--- abrupt_termination.c
+++ abrupt_termination.c
@@ -2,11 +2,12 @@ int f(int x) {
   
   while (x > 0) {
     
-    /*@ breaks x % 11 == 0 ;
-      @ continues (x+1) % 11 != 0 && x % 7 == 0; 
+    /*@ breaks x % 11 == 0 && x == \old(x);
+      @ continues (x+1) % 11 != 0 && x % 7 == 0 && x == \old(x)-1; 
       @ returns (\result+2) % 11 != 0 && (\result+1) % 7 != 0 
-      @         && \result % 5 == 0;
-      @ ensures (x+3) % 11 != 0 && (x+2) % 7 != 0 && (x+1) % 5 != 0;
+      @         && \result % 5 == 0 && \result == \old(x)-2;
+      @ ensures (x+3) % 11 != 0 && (x+2) % 7 != 0 && (x+1) % 5 != 0
+      @         && \result == \old(x)-3;
       @*/
     {
       if (x % 11 == 0) break;
