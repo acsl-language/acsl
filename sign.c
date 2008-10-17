bfbaf271bf53fc7453cc583ece0494e78e97a211
Claude Marché <claude.marche@inria.fr>
2008-10-17 13:28:36 +0000

    doc

--- sign.c
+++ sign.c
@@ -1,5 +1,8 @@
-/*@ logic integer sign(real x);
-  @ 
-  @ axiom sign_pos: \forall real x; x >= 0 ==> sign(x) == 1;
-  @ axiom sign_neg: \forall real x; x <= 0 ==> sign(x) == -1;
+
+
+/*@ axiomatic sign {
+  @   logic integer sign(real x);
+  @   axiom sign_pos: \forall real x; x >= 0 ==> sign(x) == 1;
+  @   axiom sign_neg: \forall real x; x <= 0 ==> sign(x) == -1;
+  @ }
   @*/
