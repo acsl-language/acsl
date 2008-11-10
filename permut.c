ade282db2bc125cef9fae63a11f64554facb52f1
Yannick Moy <yannick.moy@lri.fr>
2008-11-10 13:33:52 +0000

    Correct bugs

--- permut.c
+++ permut.c
@@ -2,7 +2,7 @@
   @  // permut{L1,L2}(t1,t2,n) is true whenever t1[0..n-1] in state L1 
   @  // is a permutation of  t2[0..n-1] in state L2 
   @  predicate permut{L1,L2}(double t1[], double t2[], integer n);
-  @  axiom permut_refl:
+  @  axiom permut_refl{L}:
   @   \forall double t[], integer n; permut{L,L}(t,t,n);
   @  axiom permut_sym{L1,L2} :
   @   \forall double t1[], t2[], integer n; 
