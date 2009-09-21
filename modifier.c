b4ead27995ec5d609fe1ad89abbb031fe89b3aee
Patrick Baudin <patrick.baudin@cea.fr>
2009-09-21 15:13:15 +0000

    functional modifier for struct

--- modifier.c
+++ modifier.c
@@ -2,7 +2,7 @@ struct buffer { int pos ; char buf[80]; } line;
 
 /*@ requires 80 > line.pos >= 0 ;
   @ assigns line
-      \from line = { line for buf = { line.buf for [line.pos] = '\0' };
+  @   \from line = { line for .buf = { line.buf for [line.pos] = '\0' };
   @*/
 void add_eol() {
   line.buf[line.pos] = '\0' ;
