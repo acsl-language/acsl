b3ef7de483b4e7ca19bc284e8b085154fd5efc72
Virgile Prévosto <virgile.prevosto@cea.fr>
2010-01-18 19:03:18 +0000

    - bug 344: support for \let id = term; [term | pred]. \let id = pred; pred is still missing - various obscure type-checking bugs in the logic fixed

--- mutualrec.c
+++ mutualrec.c
@@ -1,13 +1,19 @@
 //NOPP-BEGIN
 int odd(int x);
 //NOPP-END
-//@ decreases n;
+/*@
+  requires n>=0;
+  decreases n;
+*/
 int even(int n) {
   if (n == 0) return 1;
   return odd(n-1);
 }
 
-//@ decreases x;
+/*@
+  requires n>=0;
+  decreases x;
+*/
 int odd(int x) {
   if (x == 0) return 0;
   return even(x-1);
