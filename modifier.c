e6c7f9d95fe3410f3350b0c5dea33a0b5f9c1d32
Patrick Baudin <patrick.baudin@cea.fr>
2010-06-01 09:20:38 +0000

    fixed bug 397

--- modifier.c
+++ modifier.c
@@ -2,7 +2,7 @@ struct buffer { int pos ; char buf[80]; } line;
 
 /*@ requires 80 > line.pos >= 0 ;
   @ assigns line
-  @   \from line = { line \with .buf = { line.buf \with [line.pos] = '\0' };
+  @   \from line = { line \with .buf = { line.buf \with [line.pos] = (char)'\0' } };
   @*/
 void add_eol() {
   line.buf[line.pos] = '\0' ;
