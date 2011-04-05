20ff02a151d8a4b3b92511980f45086c5ca4a564
Julien Signoles <julien.signoles@cea.fr>
2011-04-05 11:15:10 +0000

    e-acsl v1.5-2

---
+++ e-acsl/link.c
@@ -0,0 +1,15 @@
+struct btree {
+  int val;
+  struct btree *left, *right;
+};
+
+/*@ axiomatic btree {
+  @   predicate access(struct btree tt, struct btree init);
+  @ } */
+
+/*@ iterator struct btree *t: 
+  @   nexts t->left, t->right; 
+  @   guards \valid(t->left), \valid(t->right) */
+
+/*@ predicate is_even(struct btree *t) = 
+  @   \forall struct btree *tt; access(tt, t) ==> tt->val % 2 == 0; */
