df3a3506bd0869f54c869f1cd74bca34f1c7b5eb
Julien Signoles <julien.signoles@cea.fr>
2011-05-04 14:14:32 +0000

    fix syntax of guards in iterators

--- e-acsl/link.c
+++ e-acsl/link.c
@@ -5,7 +5,7 @@ struct btree {
 
 /*@ iterator access(_, struct btree *t): 
   @   nexts t->left, t->right; 
-  @   guards \valid(t->left), \valid(t->right) */
+  @   guards \valid(t->left), \valid(t->right); */
 
 /*@ predicate is_even(struct btree *t) = 
   @   \forall struct btree *tt; access(tt, t) ==> tt->val % 2 == 0; */
