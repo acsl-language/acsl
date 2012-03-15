bb01ff9dbe1ed3f1da03becf4c15df030bb0e4c3
Virgile Prévosto <virgile.prevosto@cea.fr>
2012-03-15 15:24:17 +0000

    example

---
+++ loop_current.c
@@ -0,0 +1,9 @@
+int f (int n) {
+  for (int i = 0; i < n; i++) {
+  /*@ assert \at(i,LoopInit) == 0; */
+  int j = 0;
+  while (j++ < i) {
+    /*@ assert \at(j,LoopInit) == 0; */
+    /*@ assert \at(j,LoopCurrent) + 1 == j; */
+  }
+}
