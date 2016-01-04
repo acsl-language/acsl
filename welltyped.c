1e08c8dc7613b19d072fcfa34b5e677825f88c95
Boris Yakobowski <boris.yakobowski@cea.fr>
2016-01-04 14:40:01 +0100

    [ACSL] Fix example for \valid_function w.r.t. legit position of volatile

--- welltyped.c
+++ welltyped.c
@@ -1,13 +1,13 @@
-int f (int x);
+int* f (int x);
 
 void main() {
-  int (*p)(int) = &f;
-  //@ assert \valid_function((int (*)(int)) p); // true
-  //@ assert \valid_function((int (*)()) p); // true (see C99 6.7.5.3:15)
-
-  //@ assert !\valid_function((volatile int (*)(int)) p);
-    // not compatible: qualifiers cannot be dropped (see C99 6.7.3:9)
+  int* (*p)(int) = &f;
+  //@ assert \valid_function((int* (*)(int)) p); // true
+  //@ assert \valid_function((int* (*)()) p); // true (see C99 6.7.5.3:15)
 
   //@ assert !\valid_function((void* (*)(int)) p);
-    // not compatible: void* and int are not compatible (see C99 6.7.5.1:2)
+    // not compatible: void* and int* are not compatible (see C99 6.7.5.1:2)
+
+  //@ assert !\valid_function((volatile int* (*)(int)) p);
+    // not compatible: qualifiers cannot be dropped (see C99 6.7.3:9)
 }
