8d8939f9ceafb366dda4cbbbacb9e3f0acd7a382
Patrick Baudin <patrick.baudin@cea.fr>
2016-02-16 10:22:36 +0100

    [ACSL] adding an example of use of the built-in type \list<T>

--- list-observer.c
+++ list-observer.c
@@ -1,39 +1,39 @@
-//@ ghost int ghost_seq;
-/*@ axiomatic SeqObserver { 
-  @   logic \list<integer> observed_seq{L} reads ghost_seq; 
+//@ ghost int ghost_trace;
+/*@ axiomatic TraceObserver { 
+  @   logic \list<integer> observed_trace{L} reads ghost_trace; 
   @ }
   @*/
 
-/*@ assigns ghost_seq;
-  @ ensures register: observed_seq == (\old(observed_seq) ^ [| a |]);
+/*@ assigns ghost_trace;
+  @ ensures register: observed_trace == (\old(observed_trace) ^ [| a |]);
   @*/
-void observe(int a);
+void track(int a);
 
-/*@ requires empty_seq: observed_seq == \Nil;
-  @ assigns ghost_seq;
-  @ ensures head_seq: \nth(observed_seq,0) == x;
-  @ behavior shortest_seq:
+/*@ requires empty_trace: observed_trace == \Nil;
+  @ assigns ghost_trace;
+  @ ensures head_seq: \nth(observed_trace,0) == x;
+  @ behavior shortest_trace:
   @   assumes no_loop_entrance: n<=0;
-  @   ensures shortest_len: \length(observed_seq) == 2;
-  @   ensures shortest_seq: observed_seq == [| x, z |];
-  @ behavior longest_seq:
+  @   ensures shortest_len: \length(observed_trace) == 2;
+  @   ensures shortest_seq: observed_trace == [| x, z |];
+  @ behavior longest_trace:
   @   assumes loop_entrance: n>0;
-  @   ensures longest_len: \length(observed_seq) == 2+n;
+  @   ensures longest_len: \length(observed_trace) == 2+n;
   @   ensures longest_seq: 
-  @     observed_seq == ([| x |] ^ ([| y |] *^ n) ^ [| z |]);
+  @     observed_trace == ([| x |] ^ ([| y |] *^ n) ^ [| z |]);
   @*/
 void loops(int n, int x, int y, int z) {
   int i;
-  //@ ghost observe(x);
-  /*@ loop assigns i, ghost_seq;
-    @ loop invariant id_min: 0<=i;
-    @ loop invariant id_max: 0<=n ? i<=n : i<=0;
+  //@ ghost track(x);
+  /*@ loop assigns i, ghost_trace;
+    @ loop invariant idx_min: 0<=i;
+    @ loop invariant idx_max: 0<=n ? i<=n : i<=0;
     @ loop invariant inv_seq: 
-    @   observed_seq == (\at(observed_seq,LoopEntry) ^ ([| y |] *^ i)) ;  
+    @   observed_trace == (\at(observed_trace,LoopEntry) ^ ([| y |] *^ i));  
     @*/
   for (i=0; i<n; i++) { 
-    //@ ghost observe(y); 
+    //@ ghost track(y); 
     ;
   }
-  //@ ghost observe(z);
+  //@ ghost track(z);
 }
