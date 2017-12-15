e9cda47abab6dcf9721b049f41a24efc4a73d7c3
Patrick Baudin <patrick.baudin@cea.fr>
2017-12-15 14:10:28 +0100

    Adding \in infix operator

--- gen_spec_with_ghost.c
+++ gen_spec_with_ghost.c
@@ -2,13 +2,13 @@
 
 /*@ assigns forbidden;
   @ ensures ! \subset(\result,\old(forbidden))
-  @   && \subset(\result,forbidden)
+  @   && \result \in forbidden
       && \subset(\old(forbidden),forbidden);
   @*/
 int gen() {
   static int x = 0;
   /*@ global invariant I: \forall integer k;
-    @    \subset(k,forbidden) ==> x > k;
+    @    k \in forbidden ==> x > k;
     @*/
   x++;
   //@ ghost forbidden = \union(x,forbidden);
