2826e2de4a4098ba91288c9b8de9f4be353fdfc6
Allan Blanchard <allan.blanchard@cea.fr>
2020-03-18 16:35:15 +0100

    Updates ghost section

--- ghost_qualifier.c
+++ ghost_qualifier.c
@@ -1,22 +1,10 @@
-int a ;
-int * p ; // a pointer to non-const
-p = &a ;   // can only point to a non-const location
-
-//@ ghost int g ;
-//@ ghost int \ghost * pg ; // a pointer to ghost
-//@ ghost pg = &g ;          // can only point to a ghost location
-
-int const c = 42 ;
-
-int const* cp ;           // a pointer to a const location
-cp = &c ;                 // can point either to a const location
-cp = &a ;                 // or to a non const location
-
-//@ ghost int * other ;   // a pointer to a non-ghost location
-//@ ghost other = & a ;  // can point either to a non-ghost location
-//@ ghost other = & g ;  // or to a ghost location
-
-// a pointer to non-const location cannot point to a const location
-int * invalid = &c ;
-// a pointer to ghost location cannot point to a non-ghost location
-//@ ghost int \ghost * invalid = & a ;
+int a ;                      // a non-ghost location
+/*@ ghost
+  int* good = &a ;           // can be referenced by a pointer to non-ghost
+  int \ghost* bad = &a ;     // cannot be referenced by a pointer to ghost
+*/
+/*@ ghost
+  int g ;                    // a ghost location
+  int \ghost* good = &a ;    // can be referenced by a pointer to ghost
+  int * bad = &a ;           // cannot be referenced by a pointer to non-ghost
+*/
