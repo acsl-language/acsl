e0da6be760bb864ecbeef5ab627fda49357cb3aa
P. Baudin <pbaudin@users.noreply.github.com>
2021-06-04 15:57:05 +0200

    fix typo in lexico.c example

--- lexico.c
+++ lexico.c
@@ -6,17 +6,17 @@ int dummy();
 /*@ predicate lexico(intpair p1, intpair p2) =
   @   \let (x1,y1) = p1 ;
   @   \let (x2,y2) = p2 ;
-  @      x1 < x2 && 0 <= x2 || 
+  @      x1 < x2 && 0 <= x2 ||
   @      x1 == x2 && 0 <= y2 && y1 < y2;
   @*/
 
 //@ requires x >= 0 && y >= 0;
 void f(int x,int y) {
   /*@ loop invariant x >= 0 && y >= 0;
-    @ loop variant (x,y) for lexico;
+    @ loop variant (y,x) for lexico;
     @*/
   while (x > 0 && y > 0) {
-    
+
     if (dummy()) {
       x--; y = dummy();
     }
